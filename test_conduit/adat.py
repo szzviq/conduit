9d709e322b09f3c811f68cd1a68daef1706b4327
Szabó-Zichy Viktória <85553978+szzviq@users.noreply.github.com>
2021-08-06 11:49:51 +0200

    Update adat.py

--- test_conduit/adat.py
+++ test_conduit/adat.py
@@ -3,32 +3,37 @@
 # from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 # import time
-# from datetime import datetime
+from datetime import datetime
 # # import pyperclip
 # import csv
 # # import ctypes
 #
-#
-# now = datetime.now()
-# act_time = now.strftime("%H%M")
-# mail = f'szzviq{act_time}@gmail.com'
+#signup data
+now = datetime.now()
+act_time = now.strftime("%H%M")
+mail = f'szzviq{act_time}@gmail.com'
 # bad_password = 'nemjo'
-# username = f'User_new1754'
-# password = 'Valami12'
+username = f'User_new1754'
+password = 'Valami12'
+#signin data
 # dummy_mail ='szzviq1754@gmail.com'
 # mail_for_mod_profile='nulltunder@ghail.com'
+
+#data for profile modification
 # pict='https://thumbs.dreamstime.com/b/goth-girl-avatar-twin-tails-flat-74541563.jpg'
 # pict2 = "https://static.productionready.io/images/smiley-cyrus.jpg"
 # mod_mail=f'modositva{act_time}@gmail.com'
 # mod_username=f'mod_nev{act_time}'
 # mod_password='Valami13'
 # bio="Én vagyok a mesebeli okos lány, hoztam is, meg nem is..."
-#
+
+#data for new article
 # title = f"Story{act_time}"
 # about = "mese"
 # write = "Storytime"
 # tag = "mese"
 #
+#data for profile modification
 # title_mod = "Nem virágok W.H.Auden"
 # about_mod = "vers"
 # write_mod = "Festékfoltként sötétlik az ég. Valami esni fog. Nem eső, nem jég. És nem virágok"
